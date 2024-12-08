69e3f266e2ee4aacce90af2cad2b79804b7f28f3
Nando717 <iverlandioedsonn@gmail.com>
2024-12-08 13:23:35 -0300

    inicializei a construção dos services

--- src/main/java/com/ms/user/models/UserModel.java
+++ src/main/java/com/ms/user/models/UserModel.java
@@ -17,5 +17,27 @@ public class UserModel implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    public UUID getUserId() {
+        return userId;
+    }
 
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/com/ms/user/services/UserService.java
+++ src/main/java/com/ms/user/services/UserService.java
@@ -1,8 +1,32 @@
 package com.ms.user.services;
 
 
+import com.ms.user.models.UserModel;
+import com.ms.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @Transactional
+    public UserModel save(UserModel userModel){
+        return userRepository.save(userModel);
+    }
+
+
+
+
+
+
+
+
 }
